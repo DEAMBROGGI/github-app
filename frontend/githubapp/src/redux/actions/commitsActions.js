a5333e3b4a3b5cb1ca819cb5ab14a8b936455412
Adrian Deambroggi <adrian.m.deambroggi@gmail.com>
2023-11-20 08:50:16 -0300

    test SocketEmit

--- frontend/githubapp/src/redux/actions/commitsActions.js
+++ frontend/githubapp/src/redux/actions/commitsActions.js
@@ -20,7 +20,7 @@ const commitsActions = {
     },
     commentsSocketConection: () => { 
         const socket = io.connect("http://localhost:5000"); 
-        return async (dispatch, getState) => {
+        return async (dispatch, getState) => { 
             socket.on('newCommit', ( response ) => { //RECIBE EL EMIT Y EL PARAMETRO 
                 //CREAR NUEVA PROP de STORE PARA PUSH DE EVENTO    
                 console.log(response)
